4b7b3f25cf7022d61f3914ef9aef8cd4e75b41cd
Martin Arroyo <marroyo7775@gmail.com>
2022-04-30 06:22:13 -0400

    Updated find_element functions from deprecated version to new version; Added logging.

--- elt_exoplanet.py
+++ elt_exoplanet.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from dotenv import dotenv_values
 import pandas as pd
@@ -11,31 +14,34 @@ import logging
 
 # Load environment variables
 conf = dotenv_values("C:\\Users\\Martin\\exoplanet\\exoplanet_analysis\\.env")
-logging.basicConfig(filename='exoplanet_elt.log',encoding='UTF-8',level=logging.DEBUG)
+logging.basicConfig(filename='log_exoplanet_etl.log',
+                    encoding='UTF-8',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Set download directory & initiate headless browser
 options = webdriver.ChromeOptions()
-prefs = {"download.default_directory":conf['DWNLD_DIR']}
+prefs = {"download.default_directory":"C:\\Users\\Martin\\exoplanet"}
 options.add_experimental_option("prefs",prefs)
 options.headless = True
 try:
     logging.info('Getting CSV file')
-    driver = webdriver.Chrome(executable_path=conf['DRIVER'],options=options)
-
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
     # Get data page, check all options, & download CSV
     driver.get("http://exoplanet.eu/catalog/all_fields/#")
-
-    data_filter = driver.find_element_by_css_selector('.filter_catalog_form')
-    status_button = data_filter.find_element_by_css_selector('.submit_btn_inline')
-    checkbox = data_filter.find_element_by_css_selector('.filter_checkbox')
-    download = driver.find_element_by_link_text('CSV')
+    data_filter = driver.find_element(By.CSS_SELECTOR,'.filter_catalog_form')
+    status_button = data_filter.find_element(By.CSS_SELECTOR,'.submit_btn_inline')
+    checkbox = data_filter.find_element(By.CSS_SELECTOR,'.filter_checkbox')
+    download = driver.find_element(By.XPATH,'/html/body/h1/span/a[2]')
     (ActionChains(driver).move_to_element(data_filter)
                         .click(status_button)
                         .click(checkbox)
                         .click(download)
                         .perform())
 except Exception as e:
-    logging.debug(f"Something happened with the web driver: {str(e)}")
+    logging.exception(f"Error occurred while generating web driver")
+    raise
+
     
 # Check for file every 60 seconds until it is found or we waited 5 minutes
 tries = 5
@@ -50,40 +56,41 @@ if tries == 0:
 
 # Get the latest updates & load into database
 exoplanet_data = pd.read_csv(conf['DATA_FILE'])
+exoplanet_data = exoplanet_data.rename(columns={'# name':'planet_name'})
 exoplanet_data['updated'] = (pd.to_datetime(exoplanet_data['updated'])
                                .apply(lambda row: row.date()))
 last_updated = exoplanet_data['updated'].max()
 inserts = exoplanet_data[exoplanet_data['updated'] == last_updated]
-
-db_connection = psycopg2.connect(dbname=conf['DB_NAME']
-                                ,user=conf['USER']
-                                ,password=conf['PASS']
-                                ,host=conf['HOST']
-                                ,port=conf['PORT'])
-
 engine = sqlalchemy.create_engine(f"postgresql://{conf['USER']}"
                                 + f":{conf['PASS']}@{conf['HOST']}" 
                                 + f":{conf['PORT']}/{conf['DB_NAME']}")
-
 # Add new data if it is newer than the last updated date
-latest_date = pd.read_sql("""SELECT MAX(updated::DATE) 
-                                    AS updated 
-                             FROM exoplanet_stage.exoplanets"""
-                          ,db_connection)
+latest_date = pd.read_sql("""SELECT MAX(updated::DATE) AS updated 
+                             FROM exoplanet.exoplanet_data""",
+                          engine)
 
 if inserts['updated'].max() >  latest_date['updated'].max():
     try:
-        inserts.to_sql('exoplanets'
-                    ,engine
-                    ,schema='exoplanet_stage'
-                    ,if_exists='append'
-                    ,chunksize=1000
-                    ,method='multi')
-    # Delete CSV from folder
-        os.remove(conf['DATA_FILE'])
+        inserts.to_sql('exoplanet_data',
+                        engine,
+                        schema='exoplanet',
+                        if_exists='append',
+                        chunksize=1000,
+                        method='multi')
+        logging.info('New data has been inserted')
     except Exception as err:
-        print("Error occurred while inserting to db: ",str(err))
+        logging.exception("Error occurred while inserting to db: ")
+        raise
+        
+        
+# Delete CSV from folder
+try:
+    os.remove(conf['DATA_FILE'])
+    logging.info("CSV file has been removed")
+except:
+    logging.exception("Error occurred while trying to remove CSV file")
 
+logging.info('Process has completed successfully!')
 
 
 
